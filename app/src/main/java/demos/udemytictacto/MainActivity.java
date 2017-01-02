7c4806a8b98c923e40252ced8fe75dd6b695d300
Matthew Frampton <mattcmf@gmail.com>
2017-01-02 08:59:13 +0000

    Updated fields got grid and tidying image locators

--- app/src/main/java/demos/udemytictacto/MainActivity.java
+++ app/src/main/java/demos/udemytictacto/MainActivity.java
@@ -14,16 +14,6 @@ import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-import static demos.udemytictacto.R.id.grumpyCatLeftBottom;
-import static demos.udemytictacto.R.id.grumpyCatLeftMiddle;
-import static demos.udemytictacto.R.id.grumpyCatLeftTop;
-import static demos.udemytictacto.R.id.grumpyCatMiddleBottom;
-import static demos.udemytictacto.R.id.grumpyCatMiddleMiddle;
-import static demos.udemytictacto.R.id.grumpyCatMiddleTop;
-import static demos.udemytictacto.R.id.grumpyCatRightBottom;
-import static demos.udemytictacto.R.id.grumpyCatRightMiddle;
-import static demos.udemytictacto.R.id.grumpyCatRightTop;
-
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class MainActivity extends AppCompatActivity {
 
@@ -32,14 +22,6 @@ public class MainActivity extends AppCompatActivity {
     Players kitten;
     Players grumpy;
     boolean clickedFlagGridLeftTop = false;
-    boolean clickedFlagGridMiddleTop = false;
-    boolean clickedFlagGridRightTop = false;
-    boolean clickedFlagGridLeftMiddle = false;
-    boolean clickedFlagGridMiddleMiddle = false;
-    boolean clickedFlagGridRightMiddle = false;
-    boolean clickedFlagGridLeftBottom = false;
-    boolean clickedFlagGridMiddleBottom = false;
-    boolean clickedFlagGridRightBottom = false;
 
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
@@ -61,100 +43,27 @@ public class MainActivity extends AppCompatActivity {
         grumpy = new Players();
         grumpy.PlayerName = "Grumpy";
 
-
-        ImageView iv0, iv1, iv2, iv3, iv4, iv5, iv6, iv7, iv8;
-
-        iv0 = (ImageView) findViewById(grumpyCatLeftBottom);
-        iv1 = (ImageView) findViewById(grumpyCatLeftMiddle);
-        iv2 = (ImageView) findViewById(grumpyCatLeftTop);
-        iv3 = (ImageView) findViewById(grumpyCatMiddleTop);
-        iv4 = (ImageView) findViewById(grumpyCatMiddleMiddle);
-        iv5 = (ImageView) findViewById(grumpyCatMiddleBottom);
-        iv6 = (ImageView) findViewById(grumpyCatRightTop);
-        iv7 = (ImageView) findViewById(grumpyCatRightMiddle);
-        iv8 = (ImageView) findViewById(grumpyCatRightBottom);
-
-
-        ImageView[] IMGS = {iv0, iv1, iv2, iv3, iv4, iv5, iv6, iv7, iv8};
-
-        IMGS[0] = iv0;
-        IMGS[1] = iv1;
-        IMGS[2] = iv2;
-        IMGS[3] = iv3;
-        IMGS[4] = iv4;
-        IMGS[5] = iv5;
-        IMGS[6] = iv6;
-        IMGS[7] = iv7;
-        IMGS[8] = iv8;
-
-
-        for (ImageView img : IMGS) {
-            img.animate().translationY(-1500f);
-        }
-
-
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void PlaceCounter(View view) {
-
-        switch (view.getId()) {
-
-            case R.id.gridLeftTop:
-                //TODO: This will perform animation on the Rick image, but if I instead call placecounter on
-                // the cat image (and set its source to blank to start with) then I don't need to specify the grumpy cat every time.
-                if (!clickedFlagGridLeftTop) DropCounter((ImageView) view, 1);
-                clickedFlagGridLeftTop = true;
-                break;
-            case R.id.gridMiddleTop:
-                //TODO: Need to add tagging to activity_main.xml to get position
-                //Counter.getTag
-                if (!clickedFlagGridMiddleTop) DropCounter((ImageView) findViewById(grumpyCatMiddleTop), 2);
-                clickedFlagGridMiddleTop = true;
-                break;
-            case R.id.gridRightTop:
-                if (!clickedFlagGridRightTop) DropCounter((ImageView) findViewById(grumpyCatRightTop), 3);
-                clickedFlagGridRightTop = true;
-                break;
-            case R.id.gridLeftMiddle:
-                if (!clickedFlagGridLeftMiddle) DropCounter((ImageView) findViewById(grumpyCatLeftMiddle), 4);
-                clickedFlagGridLeftMiddle = true;
-                break;
-            case R.id.gridMiddleMiddle:
-                if (!clickedFlagGridMiddleMiddle) DropCounter((ImageView) findViewById(grumpyCatMiddleMiddle), 5);
-                clickedFlagGridMiddleMiddle = true;
-                break;
-            case R.id.gridRightMiddle:
-                if (!clickedFlagGridRightMiddle) DropCounter((ImageView) findViewById(grumpyCatRightMiddle), 6);
-                clickedFlagGridRightMiddle = true;
-                break;
-            case R.id.gridLeftBottom:
-                if (!clickedFlagGridLeftBottom) DropCounter((ImageView) findViewById(grumpyCatLeftBottom), 7);
-                clickedFlagGridLeftBottom = true;
-                break;
-            case R.id.gridMiddleBottom:
-                if (!clickedFlagGridMiddleBottom) DropCounter((ImageView) findViewById(grumpyCatMiddleBottom), 8);
-                clickedFlagGridMiddleBottom = true;
-                break;
-            case R.id.gridRightBottom:
-                if (!clickedFlagGridRightBottom) DropCounter((ImageView) findViewById(grumpyCatRightBottom), 9);
-                clickedFlagGridRightBottom = true;
-                break;
-        }
+        if (!clickedFlagGridLeftTop) DropCounter((ImageView) view);
+                //clickedFlagGridLeftTop = true;
     }
 
-    private void DropCounter(ImageView view, int counterPos) {
+    private void DropCounter(ImageView view) {
 
         //TODO: Need to add flag to check whether game is active
 
-        //TODO: Setting the ByX value to 9000 here will avoid the double tap goes off screen bug
-        // It does require having the image set to null source though.
-
+        view.setTranslationY(-1200f);
         String currentPlayer = SelectCounter(view);
-        view.animate().translationYBy(1500f).setDuration(500);
+        view.animate().translationYBy(1200f).setDuration(500);
+        int counterPos =Integer.parseInt(String.valueOf(view.getTag()));
         RecordPlayerCounterPos(counterPos, currentPlayer);
         ChangePlayerTurn();
         String kittenStats = game.CheckForWinner(kitten);
--- app/src/main/java/demos/udemytictacto/Players.java
+++ app/src/main/java/demos/udemytictacto/Players.java
@@ -3,14 +3,13 @@ package demos.udemytictacto;
 import android.support.v7.app.AppCompatActivity;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by matthewframpton on 28/12/2016.
  */
 
 public class Players  extends AppCompatActivity {
-    List<Integer> counterPositions = new ArrayList<Integer>();
+    ArrayList counterPositions = new ArrayList();
     String PlayerName = "";
 }
 
